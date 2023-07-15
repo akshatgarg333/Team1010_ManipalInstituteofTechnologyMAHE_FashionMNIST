a589934375f75f7ef5214d177b802b500e0163dc
Akshat Garg <67066658+akshatgarg333@users.noreply.github.com>
2023-07-15 13:13:19 +0530

    Update TorchCNN.py

--- Models/TorchCNN.py
+++ Models/TorchCNN.py
@@ -1,1 +1,65 @@
+#creating the model
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        self.dropout1 = nn.Dropout(0.3)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        self.dropout2 = nn.Dropout(0.3)
+
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        self.batchnorm3 = nn.BatchNorm2d(128)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        self.dropout3 = nn.Dropout(0.25)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(128 * 3 * 3, 256)
+        self.relu4 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(256, 64)
+        self.relu5 = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(64, 10)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.maxpool3(x)
+
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.relu5(x)
+        x = self.dropout2(x)
+
+        x = self.fc3(x)
+        x = self.softmax(x)
+
+        return x
 
